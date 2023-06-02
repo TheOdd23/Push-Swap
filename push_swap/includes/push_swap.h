4031e31369c5d3b11c57689ff2f63716e4c27605
Anthony <100093373+TheOdd23@users.noreply.github.com>
2023-06-02 12:08:41 -0400

    Update push_swap.h

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -16,21 +16,21 @@
 # include "../libft/libft.h"
 # include <limits.h>
 
-typedef struct t_l
+typedef struct t_intlist
 {
 	int			content;
-	int			in;
-	struct t_l	*next;
-}	t_l;
+	int			index;
+	struct t_intlist	*next;
+}	t_intlist;
 
-typedef struct t_lim
+typedef struct t_limits
 {
-	int		l2; //limit 2
-	int		l; //limit 1
-	int		m; //median
-	int		s; //size
-	int		i; //index
-}	t_lim;
+	int		first_limit; //limit 2
+	int		second_limit; //limit 1
+	int		median; //median
+	int		size; //size
+	int		index; //index
+}	t_limits;
 
 void		swap(t_l **a, char c);
 void		push(t_l **s1, t_l **s2, char c);
@@ -40,11 +40,11 @@ void		swap_ab(t_l **a, t_l **b, char c);
 void		reverse_rotate_ab(t_l **a, t_l **b, char c);
 void		rotate_ab(t_l **a, t_l **b, char c);
 long long	ft_atoll(const char *str);
-int			in_order(int ac, char **av, int *len);
+int		in_order(int ac, char **av, int *len);
 void		check_errors(int *ac, char ***av, int *len);
 void		indexation(t_l **a);
 void		push_swap(t_l *a, t_l *b, int size);
-int			ft_nb_args(char const *str, char c);
+int		nb_args(char const *str, char c);
 void		ultra_simple_swap(t_l *a, t_l *b, int size);
 void		do_op_1_a(t_l **a, t_l **b, t_lim l, int var);
 void		do_op_2_a(t_l **a, t_l **b, t_lim l, int var);
@@ -52,7 +52,7 @@ void		do_op_1_b(t_l **a, t_l **b, t_l **head, t_lim *l);
 void		do_op_2_b(t_l **a, t_l **b, t_l **head, t_lim *l);
 void		do_op_3_b(t_l **a, t_l **b, t_l **head, t_lim *l);
 void		del_tab(char **splitted, int ac);
-void		del_lists(t_l *list);
-int			ft_intlistsize(t_l *lst);
+void		del_lists(t_intlist *list);
+int		intlist_size(t_intlist *lst);
 
 #endif
