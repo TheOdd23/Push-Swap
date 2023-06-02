75348d3c72a163ca1c5ca4af865679e567f788f9
Anthony <100093373+TheOdd23@users.noreply.github.com>
2023-06-02 12:03:37 -0400

    Update check_errors.c

--- push_swap/srcs/check_errors.c
+++ push_swap/srcs/check_errors.c
@@ -14,8 +14,8 @@
 
 long long	ft_atoll(const char *str)
 {
-	int			i;
-	int			neg;
+	int		i;
+	int		neg;
 	long long	nb;
 
 	i = 0;
@@ -43,8 +43,8 @@ long long	ft_atoll(const char *str)
 
 int	check_doublons(int ac, char **av, int *len)
 {
-	int	res;
-	int	res2;
+	int	nb;
+	int	nb2;
 	int	i;
 	int	ii;
 
@@ -53,12 +53,12 @@ int	check_doublons(int ac, char **av, int *len)
 		i = 1;
 	while (i < ac)
 	{
-		res = ft_atoi(av[i]);
+		nb = ft_atoi(av[i]);
 		ii = i + 1;
 		while (av[ii])
 		{
-			res2 = ft_atoi(av[ii]);
-			if (res2 == res)
+			nb2 = ft_atoi(av[ii]);
+			if (nb2 == nb)
 			{
 				write(2, "Error\n", 6);
 				return (0);
@@ -73,24 +73,24 @@ int	check_doublons(int ac, char **av, int *len)
 int	check_digit(int ac, char **av, int *len)
 {
 	int	i;
-	int	j;
+	int	ii;
 
 	i = 0;
 	if (*len == -1)
 		i = 1;
 	while (i < ac)
 	{
-		j = 0;
-		if (av[i][j] == '-' && ft_isdigit(av[i][1]) == 1)
-			j++;
-		while (av[i][j])
+		ii = 0;
+		if (av[i][ii] == '-' && ft_isdigit(av[i][1]) == 1)
+			ii++;
+		while (av[i][ii])
 		{
-			if ((av[i][j] < '0' || av[i][j] > '9') && av[i][j])
+			if ((av[i][ii] < '0' || av[i][ii] > '9') && av[i][ii])
 			{
 				write(2, "Error\n", 6);
 				return (0);
 			}
-			j++;
+			ii++;
 		}
 		i++;
 	}
@@ -99,7 +99,7 @@ int	check_digit(int ac, char **av, int *len)
 
 int	check_int(int ac, char **av, int *len)
 {
-	long	res;
+	long	nb;
 	int		i;
 
 	i = 0;
@@ -107,8 +107,8 @@ int	check_int(int ac, char **av, int *len)
 		i = 1;
 	while (i < ac)
 	{
-		res = ft_atoll(av[i]);
-		if (res < -2147483648 || res > 2147483647)
+		nb = ft_atoll(av[i]);
+		if (nb < INT_MIN || nb > INT_MAX)
 		{
 			write(2, "Error\n", 6);
 			return (0);
